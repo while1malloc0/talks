75bc23beaad0dfbe537cecf96ec5fea37404318b
John Turner <while1malloc0@gmail.com>
2018-11-18 21:46:48 +0000

    Clean up example

--- code_review_culture/examples/example.py
+++ code_review_culture/examples/example.py
@@ -1,7 +1,4 @@
 def read_ips_from_file():
-    # snip...
-
-def my_function():
-    ips = read_ips_from_file()
-    for ip in range(ips):
-        convert_to_ipv6(ip)
+    with open(IP_FILE) as f:
+        content = f.readlines()
+    return content
