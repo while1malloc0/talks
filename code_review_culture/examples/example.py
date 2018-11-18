d5e82f45b39d7dfa2794d920489a0a02f44fa9e9
John Turner <while1malloc0@gmail.com>
2018-11-18 21:55:42 +0000

    Add variable naming example

--- code_review_culture/examples/example.py
+++ code_review_culture/examples/example.py
@@ -2,3 +2,9 @@ def read_ips_from_file():
     with open(IP_FILE) as f:
         content = f.readlines()
     return content
+
+def process_ips():
+    ipArray = read_ips_from_file()
+
+    for ip in ipArray:
+        process(ip)
